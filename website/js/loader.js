fe5db379c8a340e40e0493eab654e315f68f657f
spipu <spipu45@gmail.com>
2026-05-13 18:49:34 +0200

    Add loader.js to load all JS in correct order, single script tag in each HTML

---
+++ website/js/loader.js
@@ -0,0 +1,12 @@
+[
+    'js/matrix.js',
+    'js/light.js',
+    'js/object3d.js',
+    'js/zBuffer.js',
+    'js/object3dRendererFast.js',
+    'js/object3dRendererFull.js',
+    'js/object3dRenderer.js',
+    'js/engine3d.js',
+    'js/object3dFactory.js',
+    'js/input.js',
+].forEach(src => document.write('<script src="' + src + '"><\/script>'));
