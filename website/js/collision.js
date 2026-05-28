f2afe44f92b16d40a628d26f37588dfde5b895d9
spipu <spipu45@gmail.com>
2026-05-28 23:55:22 +0200

    Doom door instances: world-space geometry, full-height panels, static map cleanup

--- website/js/collision.js
+++ website/js/collision.js
@@ -15,6 +15,7 @@ class Collision {
         const obj = instance.getObject();
         const localTris = [];
         for (const fc of obj.faceList) {
+            if (fc[9]) continue;
             const A = obj.ptOrigin[fc[0]], B = obj.ptOrigin[fc[1]], C = obj.ptOrigin[fc[2]];
             localTris.push([[A[0],A[1],A[2]], [B[0],B[1],B[2]], [C[0],C[1],C[2]]]);
         }
