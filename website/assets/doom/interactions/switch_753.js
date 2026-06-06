7cf2e8e86999a7f1b95cfbdef878c39c52f2e970
spipu <spipu45@gmail.com>
2026-06-06 02:49:16 +0200

    Switch→lift linkage: interaction system wired to targets

--- website/assets/doom/interactions/switch_753.js
+++ website/assets/doom/interactions/switch_753.js
@@ -6,6 +6,9 @@ class Switch753Interaction extends SwitchInteraction {
     _triggerOn(instance) {
         const obj = instance.getObject();
         obj.faceList.forEach(fc => { fc.textureId = obj.getTextureId(2); });
+        loader.instances().getByCode('lift_129').start();
+        loader.instances().getByCode('lift_76').start();
+        loader.instances().getByCode('lift_126').start();
     }
     _triggerOff(instance) {
         const obj = instance.getObject();
