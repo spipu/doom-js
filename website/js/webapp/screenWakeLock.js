9980c54ade77a856cb761cb7a35e69cce502bc06
spipu <spipu45@gmail.com>
2026-06-11 03:16:32 +0200

    Add ScreenWakeLock to doom (from escape-game), bump engine to v2.000

---
+++ website/js/webapp/screenWakeLock.js
@@ -0,0 +1,44 @@
+class ScreenWakeLock {
+    /** @type {boolean} */ isSupported;
+    /**                 */ wakeLock;
+
+    constructor() {
+        this.isSupported = ('wakeLock' in navigator);
+        this.wakeLock = null;
+    }
+
+    init() {
+        this.ask();
+        document.addEventListener('visibilitychange', this.handleVisibilityChange.bind(this));
+    }
+
+    async ask() {
+        if (!this.isSupported) {
+            return;
+        }
+        try {
+            this.wakeLock = await navigator.wakeLock.request("screen");
+        } catch (err) {
+            console.log(`Error on Wake Lock - ${err.name}, ${err.message}`);
+        }
+    }
+
+    release() {
+        if (this.wakeLock === null) {
+            return;
+        }
+
+        let that = this;
+        that.wakeLock.release().then(
+            () => {
+                that.wakeLock = null;
+            }
+        )
+    }
+
+    handleVisibilityChange() {
+        if (this.wakeLock !== null && document.visibilityState === 'visible') {
+            this.ask();
+        }
+    }
+}
